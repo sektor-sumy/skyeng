1a693da664ee647b5c8f4de45f9295c1bcb17f1d
Mihail Bogdanov <sektorsumy@gmail.com>
2015-03-16 09:58:57 +0300

    remove folder

--- zendesk/resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('app')
-
-@section('content')
-	<div class="list-group">
-		@foreach($data as $user)
-			<a href="/{{$user->id}}/show" class="list-group-item"> {{$user->name}}</a></li>
-		@endforeach
-	</div>
-@endsection
